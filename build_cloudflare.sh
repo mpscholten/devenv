a5aa7439e40a0a0e9e2866122105b41f0a02d907
Domen Kožar <domen@dev.si>
2023-04-25 16:13:26 +0100

    fix CF pages

--- build_cloudflare.sh
+++ build_cloudflare.sh
@@ -1,3 +1,3 @@
-locale 
+pip install -e .
 pip install git+https://${GH_TOKEN}@github.com/squidfunk/mkdocs-material-insiders.git@9.1.5-insiders-4.32.4
 mkdocs build --config-file mkdocs.insiders.yml
